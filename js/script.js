9262087769fb30d240d69e2e6b0c6e4cbc392ce5
Chris Lee <nobleelement@yahoo.com.sg>
2024-01-15 01:29:22 +0800

    Edu and Exp

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-function showNavList(){
-    var a = document.getElementById("nav-list");
-    if (window.getComputedStyle(a).display === "none"){
-        a.style.display = "inline-block";
-    }
-    else{
-        a.style.display = "none"; //Try and make this a class
-    }
+window.onload = function(){ 
+   let icon = document.querySelector(".icon");
+   let links = document.querySelector(".nav-list");
+ 
+   icon.onclick = function(){
+      if(links.style.maxHeight == 0){
+         links.style.maxHeight = links.scrollHeight + "px";
+      }else{
+         links.style.maxHeight = "";
+      }
+   }
 }
